5c8e22b247d7de50c595b659c67c22c799f75f48
Nguyen Ngoc Phu <phunn@cpu-phunn.(none)>
2015-07-24 11:46:51 +0700

    redirect error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,14 @@
 class ErrorsController < ApplicationController
+
   def file_not_found
+  	redirect_to root_path
   end
 
   def unprocessable
+  	redirect_to root_path
   end
 
   def internal_server_error
+  	redirect_to root_path
   end
 end
